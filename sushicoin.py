a384c0c7bd8231e137030d0a19523affd29c3fe7
Sushant Singh <sushantvvsingh@gmail.com>
2021-06-14 20:14:59 +0530

    modification

--- sushicoin.py
+++ sushicoin.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 import requests
 from uuid import uuid4
 from urllib.parse import urlparse
-
+from flask_cors import CORS, cross_origin
 
 #creating blockchain
 class Blockchain:
@@ -16,9 +16,10 @@ class Blockchain:
         self.transactions = []
         self.mempool = []
         self.mempoolLastUpdated = None
+        
         #creating Genesis Block
         self.CreateBlock(proof = 1, previousHash = '0')
-        self.nodes = set()
+        self.nodes = []
         
     def CreateBlock(self, proof, previousHash):
         block = {'index' : len(self.chain)+1,
@@ -65,14 +66,16 @@ class Blockchain:
         return True
     
     def AddTransaction(self, sender, reciever, amount):
-        self.transactions.append({'sender' : sender,
+        self.mempool.append({'sender' : sender,
                                   'reciever' : reciever,
                                   'amount' : amount})
         previousBlock = self.GetLastBlock()
         return previousBlock['index'] + 1
     
     def AddNode(self, address):
-        self.nodes.add(urlparse(address).netloc)
+        nodeAddress = urlparse(address).netloc
+        if nodeAddress not in self.nodes:
+            self.nodes.append(nodeAddress)
         
     def ReplaceChain(self):
         currentChainLength = len(self.chain)
@@ -102,32 +105,54 @@ class Blockchain:
                         self.mempoolLastUpdated = lastUpdationTime
                         self.mempool = nodeMempool
                     
-    
+    def UpdateNodes(self):
+        for node in self.nodes:
+            if(nodeIp not in node):
+                response = requests.get(f'http://{node}/getNodes')
+                if response.status_code == 200:
+                    nodes = response.json()['nodes']
+                    length = response.json()['length']
+                    if length > len(self.nodes):
+                        self.nodes = nodes
+                        
+                        
     def SendRequestToUpdateMempool(self):
         for node in self.nodes:
             if(nodeIp not in node):
                 requests.get(f'http://{node}/updateMempool')
         
+    def SendRequestToUpdateNodes(self):
+        for node in self.nodes:
+            if(nodeIp not in node):
+                requests.get(f'http://{node}/updateNodes')
+        
+        
+    def RemoveTransactionFromMempool(self,transactions):
+        self.mempool = [transaction for transaction in self.mempool if transaction not in transactions]
 #mining 
         
 app = Flask(__name__)
 #app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+CORS(app, support_credentials=True)
 nodeAddress = str(uuid4()).replace('-','')
 nodeIp = requests.get('http://ip.42.pl/raw').text
-
+nodePort = 5000
 blockchain = Blockchain()
+
 
 @app.route('/mineBlock', methods=['GET'])
 def MineBlock():
+    json = request.get_json()
+    blockchain.transactions = json.get('transactions')
+    if(len(blockchain.transactions) is 0 or len(blockchain.transactions) > 1500):
+        return 'Number of transaction not in range, valid range is between 1 to 1500', 400
     previousBlock = blockchain.GetLastBlock()
     previousProof = previousBlock['proof']
     currentProof = blockchain.ProofOfWork(previousProof)
     previousHash = blockchain.Hash(previousBlock)
     blockchain.AddTransaction(sender = nodeAddress, reciever = 'Me', amount = 1)
     block = blockchain.CreateBlock(currentProof,previousHash)
-    blockchain.ReplaceChain()
-    blockchain.SendRequestToUpdateMempool()
     response = {'message' : 'Block mined succesfully !!!',
                 'index' : block['index'],
                 'timestamp' : block['timestamp'],
@@ -150,28 +175,32 @@ def IsChainValid():
     response = {'message' : 'Blockchain is Valid' if isChainValid else 'Blockchain is not valid'}
     return jsonify(response), 200
 
-@app.route('/addTransaction', methods=['POST'])
-def AddTransaction():
+@app.route('/addTransactionToMempool', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def AddTransactionToMempool():
     blockchain.mempoolLastUpdated = str(dt.datetime.now())
     json = request.get_json()
-    transactionKeys = ['sender', 'reciever', 'amount']
+    transactionKeys = ['timestamp', 'sender', 'reciever', 'amount']
     if not all (key in json for key in transactionKeys):
         return 'Some fields are missing', 400
-    index = blockchain.AddTransaction(json['sender'], json['reciever'], json['amount'])
-    response = {'message': f'This transaction will be added to Block {index}'}
+    blockchain.AddTransaction(json['sender'], json['reciever'], json['amount'])
+    response = {'message': f'Transaction added to mempool'}
     return jsonify(response), 201
 
 @app.route('/addNode', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def AddNode():
     json = request.get_json()
     address = json.get('address')
-    response = requests.get(f'http://{address}/getNodes')
-    nodes = response.json()['nodes']
-    if nodes in None:
-        return "No node", 400
+    print('aaa',address)
+    try:
+        response = requests.get(f'http://{address}/getNodes')
+        nodes = response.json()['nodes']
+    except Exception as e:
+        nodes = []
     for node in nodes:
         blockchain.AddNode(node)
-    blockchain.ReplaceChain()
+    blockchain.AddNode(f'http://{nodeIp}:{nodePort}')
     response = {'message': 'All the nodes are now connected. The Sushicoin Blockchain now contains the following nodes:',
                 'total_nodes': list(blockchain.nodes),
                 'chain' : blockchain.chain}
@@ -186,7 +215,8 @@ def ReplaceChain():
 
 @app.route('/getNodes', methods=['GET'])
 def GetNodes():
-    response = {'nodes' : blockchain.nodes}
+    response = {'nodes' : blockchain.nodes,
+                'length' : len(blockchain.nodes)}
     return jsonify(response), 200
 
 @app.route('/getMempool', methods=['GET'])
@@ -202,9 +232,18 @@ def UpdatetMempool():
                 'mempool' : blockchain.mempool}
     return jsonify(response), 200
 
-app.run(host = '0.0.0.0', port = 5000)
+@app.route('/updateNodes', methods=['GET'])
+def UpdatetNodes():
+    blockchain.UpdateNodes()
+    response = {'message' : 'Nodes updated',
+                'mempool' : blockchain.mempool}
+    return jsonify(response), 200
 
+@app.route('/removeTransactionFromMempool', methods=['GET'])
+def RemoveTransactionFromMempool():
+    blockchain.RemoveTransactionFromMempool()
+    response = {'message' : 'mempool updated',
+                'mempool' : blockchain.mempool}
+    return jsonify(response), 200
 
-
-
-
+app.run(host = '0.0.0.0', port = nodePort)
